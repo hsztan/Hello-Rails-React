eef869f88671240937eaa3e12d0f501cf3d216d9
Henry Nawrocki <hdev@sztanski.com>
2022-08-08 13:28:36 -0500

    Connect Redux to to Greeting Component

--- app/javascript/components/Greeting.jsx
+++ app/javascript/components/Greeting.jsx
@@ -1,13 +1,40 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 import PropTypes from 'prop-types';
+
+const GET_GREETING = 'GET_GREETING';
+
+function getGreeting() {
+  console.log('getGreeting(), Action!!');
+  return {
+    type: GET_GREETING,
+  };
+}
 
 class Greeting extends React.Component {
   render() {
-    return <React.Fragment>Greeting: {this.props.greeting}</React.Fragment>;
+    return (
+      <React.Fragment>
+        <button
+          className="getGreetingBtn"
+          onclick={() => this.props.getGreeting()}
+        >
+          Get Greeting
+        </button>
+        Greeting: {this.props.greeting}
+      </React.Fragment>
+    );
   }
 }
+
+const structuredSelector = createStructuredSelector({
+  greeting: (state) => state.greeting,
+});
+
+const mapDispatchToProps = { getGreeting };
 
 Greeting.propTypes = {
   greeting: PropTypes.string,
 };
-export default Greeting;
+export default connect(structuredSelector, mapDispatchToProps)(Greeting);
